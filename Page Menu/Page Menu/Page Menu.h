d505be911f76340ba2672f270ce27a324c72041a
Nick Baidikoff <nbaidikoff@gmail.com>
2017-08-20 11:03:18 +0300

    Add empty framework project and empty example project

---
+++ Page Menu/Page Menu/Page Menu.h
@@ -0,0 +1,19 @@
+//
+//  Page Menu.h
+//  Page Menu
+//
+//  Created by Nick Baidikoff on 8/20/17.
+//  Copyright © 2017 Nick Baidikoff. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Page Menu.
+FOUNDATION_EXPORT double Page_MenuVersionNumber;
+
+//! Project version string for Page Menu.
+FOUNDATION_EXPORT const unsigned char Page_MenuVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Page_Menu/PublicHeader.h>
+
+
